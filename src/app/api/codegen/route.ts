88305d8982e593aab963c05f3fae3aa54b3b5ee8
Anish <xevenbiswas@gmail.com>
2024-09-30 16:22:37 +0530

    chnged to gemini

--- src/app/api/codegen/route.ts
+++ src/app/api/codegen/route.ts
@@ -1,22 +1,23 @@
-// import { google } from "@ai-sdk/google";
+import { google } from "@ai-sdk/google";
 import { streamObject } from "ai";
 import { z } from "zod";
 export const dynamic = "force-dynamic";
 export const maxDuration = 30;
-import { createOpenAI } from "@ai-sdk/openai";
+// import { createOpenAI } from "@ai-sdk/openai";
 
-const groq = createOpenAI({
-  baseURL: "https://api.groq.com/openai/v1",
-  apiKey: process.env.GROQ_API_KEY,
-});
+// if u wanna use Groq or OpenAI, you can use the following code
+// const groq = createOpenAI({
+//   baseURL: "https://api.groq.com/openai/v1",
+//   apiKey: process.env.GROQ_API_KEY,
+// });
 
 export async function POST(req: Request) {
   const context = await req.json();
 
   const result = await streamObject({
-    model: groq("llama-3.1-70b-versatile"),
+    model: google("gemini-1.5-pro-latest"),
     system:
-      "You are the best code and code generator. you take the code and give the converted code in the perfect format with some bit of explanations. you will reject everything except codes.",
+      "You are the best coder and code generator. you take the code and give the converted code in the perfect format with some bit of explanations. you will reject everything except codes.",
     prompt: context,
     schemaDescription: "Code and its explanation",
     schema: z.object({
